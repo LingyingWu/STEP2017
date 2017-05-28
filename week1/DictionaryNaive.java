1e43eac1e84566434f06ae8cd5be77e0ae3b6407
LingYing Wu <LingyingWu@users.noreply.github.com>
2017-05-28 11:15:43 +0900

    Add programs

---
+++ week1/DictionaryNaive.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class DictionaryNaive {
+
+	private Map<String, String> dictionary = new TreeMap<String, String>();
+	private Set<String> combination = new HashSet<String>();
+
+	public DictionaryNaive() throws FileNotFoundException {
+		createDict();
+	}
+
+	// create a dictionary
+	// the keys are the sorted strings of each word
+	// put the words with same anagram under the same key
+	public void createDict() throws FileNotFoundException {
+		File file = new File("dictionary.words.txt");
+		Scanner sc = new Scanner(file);
+		while (sc.hasNextLine()) {
+			String word = sc.nextLine();
+			String key = sortString(word);
+			if (dictionary.containsKey(key))
+				dictionary.put(key, dictionary.get(key) + " " + word);
+			else
+				dictionary.put(key, word);
+		}
+		sc.close();
+	}
+
+	// search for the words having the same anagram with the combinations of the
+	// input characters in the dictionary
+	public String searchDict(String input) {
+		String result = "";
+		generateCombination(sortString(input), new StringBuffer(), 0);
+		for (String str : combination) {
+			if (dictionary.containsKey(str))
+				result += dictionary.get(str) + " ";
+		}
+		return result;
+	}
+
+	// generate all possible combinations of the input characters
+	public void generateCombination(String str, StringBuffer output, int index) {
+		for (int i = index; i < str.length(); i++) {
+			output.append(str.charAt(i));
+			combination.add(output.toString());
+			generateCombination(str, output, i + 1);
+			output.deleteCharAt(output.length() - 1);
+		}
+	}
+
+	// sort the characters in a given string
+	public static String sortString(String s) {
+		char[] chars = s.toCharArray();
+		Arrays.sort(chars);
+		return new String(chars);
+	}
+}
---
+++ week1/HazWordz.java
@@ -0,0 +1,78 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class HazWordz {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		// receive input
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please input the characters: ");
+		String input = sc.next().toLowerCase();
+
+		DictionaryNaive dict = new DictionaryNaive();
+		String[] candidate = dict.searchDict(input).split(" ");
+
+		System.out.println("Best result: " + findBest(candidate));
+
+		sc.close();
+	}
+
+	// find the word with the highest score in the candidate list
+	public static String findBest(String[] list) {
+		String best = "";
+		int max = 0;
+		for (int i = 0; i < list.length; i++) {
+			int x = computeScore(list[i]);
+			if (x > max) {
+				best = list[i];
+				max = x;
+			}
+		}
+		return best;
+	}
+
+	// compute the final score
+	public static int computeScore(String str) {
+		int score = 0;
+		char[] chars = str.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			switch (chars[i]) {
+			case 'a':
+			case 'b':
+			case 'd':
+			case 'e':
+			case 'g':
+			case 'i':
+			case 'n':
+			case 'o':
+			case 'r':
+			case 's':
+			case 't':
+			case 'u':
+				score += 1;
+				break;
+			case 'c':
+			case 'f':
+			case 'h':
+			case 'l':
+			case 'm':
+			case 'p':
+			case 'v':
+			case 'w':
+			case 'y':
+				score += 2;
+				break;
+			case 'j':
+			case 'k':
+			case 'q':
+			case 'x':
+			case 'z':
+				score += 3;
+				break;
+			}
+		}
+
+		return score;
+	}
+}
