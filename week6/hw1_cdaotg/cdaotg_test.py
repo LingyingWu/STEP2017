0529cce9c0a154654d2fa1349a18fc95199ca202
lingyingwu <lingyingwu115@gmail.com>
2017-07-05 13:12:08 +0900

    Modify programs

--- week6/hw1_cdaotg/cdaotg_test.py
+++ week6/hw1_cdaotg/cdaotg_test.py
@@ -4,8 +4,20 @@ import webtest
 def test_get():
 	app = webtest.TestApp(cdaotg.app)
 
-	response1 = app.get('\pata?a=cat&b=dog')
-	
+	# test when same length
+	response1 = app.get('\pata?a=123&b=456')
 	assert response1.status_int == 200
 	assert response1.content_type == 'text/html'
-	assert response1.body.contains('cdaotg')
+	assert response1.body.contains('123456')
+
+	# test when a longer than b
+	response2 = app.get('\pata?a=1234&b=56')
+	assert response2.status_int == 200
+	assert response2.content_type == 'text/html'
+	assert response2.body.contains('152634')
+
+	# test when a shorter than b
+	response1 = app.get('\pata?a=12&b=3456')
+	assert response1.status_int == 200
+	assert response1.content_type == 'text/html'
+	assert response1.body.contains('132456')
