40955a57b343ecddafb416c506454315f11b8e50
lingyingwu <lingyingwu115@gmail.com>
2017-07-06 01:09:17 +0900

    Modify programs

--- week6/hw2_trainguidance/trainguidance.py
+++ week6/hw2_trainguidance/trainguidance.py
@@ -138,7 +138,7 @@ class RoutePlanner(webapp2.RequestHandler):
 	def print_route(self, start, end):
 		intersection_line = (self.get_line(start) & self.get_line(end))
 		for line in intersection_line:
-			self.response.write('<p style="color:orange">[ %s ]</p>' % self.get_whole_line(line)['Name'])
+			self.response.write('<p style="color:orange">[ %s: ' % self.get_whole_line(line)['Name'])
 			start_index = self.get_index(start, self.get_whole_line(line)['Stations'])
 			end_index = self.get_index(end, self.get_whole_line(line)['Stations'])
 			if start_index < end_index:
@@ -146,12 +146,21 @@ class RoutePlanner(webapp2.RequestHandler):
 					route = self.get_whole_line(line)['Stations'][end_index:]
 					route.append(self.get_whole_line(line)['Stations'][1:start_index+1])
 					route.reverse()
+					self.response.write('up ]')
 				else: 
 					route = self.get_whole_line(line)['Stations'][start_index:end_index+1]
+					self.response.write('down ]')
 			else:
-				route = self.get_whole_line(line)['Stations'][end_index:start_index+1]
-				route.reverse()
+				if (start_index-end_index) > (self.get_station_num(line)/2):
+					route = self.get_whole_line(line)['Stations'][end_index:]
+					route.append(self.get_whole_line(line)['Stations'][1:start_index+1])
+					self.response.write('down ]')
+				else:
+					route = self.get_whole_line(line)['Stations'][end_index:start_index+1]
+					route.reverse()
+					self.response.write('up ]')
 
+			self.response.write('</p>')
 			for station in route:
 				self.response.write('>> %s<br>' % station)
 
