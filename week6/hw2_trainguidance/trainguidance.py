083fea9805317d6bb1c77e0ca1e0f0a839caeba0
lingyingwu <lingyingwu115@gmail.com>
2017-07-07 02:56:15 +0900

    Modify programs

--- week6/hw2_trainguidance/trainguidance.py
+++ week6/hw2_trainguidance/trainguidance.py
@@ -285,7 +285,7 @@ class RoutePlanner(webapp2.RequestHandler):
 	def print_route(self, start, end):
 		intersection_line = (self.get_line(start) & self.get_line(end))
 		m = sys.maxint
-		line = set([])
+		line = intersection_line[0]
 		for l in intersection_line:
 			start_index = self.get_index(start, self.get_whole_line(line)['Stations'])
 			end_index = self.get_index(end, self.get_whole_line(line)['Stations'])
