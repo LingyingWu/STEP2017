9790f5486103e14aaba8968f086c0e413765eef0
lingyingwu <lingyingwu115@gmail.com>
2017-07-07 03:31:34 +0900

    Modify programs

--- week6/hw2_trainguidance/trainguidance.py
+++ week6/hw2_trainguidance/trainguidance.py
@@ -28,7 +28,8 @@ class MainPage(webapp2.RequestHandler):
 				self.response.write('<option>%s</option>' % station)
 		self.response.write('</select></h3><input type="submit" value="  Search Route  "></form>')
 
-		self.response.write('<br><h3>Reference: <h3><a href="http://fantasy-transit.appspot.com/map">Map</a>')
+		self.response.write('<br><h3>Reference:<h3><ul>')
+		self.response.write('<li><a href="http://fantasy-transit.appspot.com/map">Map</a><li></ul>')
 
 		self.response.write('<h3>Alternate worlds:</h3><ul>')
 		self.response.write('<li><a href="../">Tokyo</a></li>')
@@ -64,7 +65,8 @@ class AliceMainPage(webapp2.RequestHandler):
 				self.response.write('<option>%s</option>' % station)
 		self.response.write('</select></h3><input type="submit" value="  Search Route  "></form>')
 
-		self.response.write('<br><h3>Reference: <h3><a href="http://alice.fantasy-transit.appspot.com/map">Map</a>')
+		self.response.write('<br><h3>Reference:<h3><ul>')
+		self.response.write('<li><a href="http://alice.fantasy-transit.appspot.com/map">Map</a><li></ul>')
 
 		self.response.write('<h3>Alternate worlds:</h3><ul>')
 		self.response.write('<li><a href="../">Tokyo</a></li>')
@@ -100,9 +102,10 @@ class NausicaaMainPage(webapp2.RequestHandler):
 				self.response.write('<option>%s</option>' % station)
 		self.response.write('</select></h3><input type="submit" value="  Search Route  "></form>')
 
-		self.response.write('<br><a href="http://nausicaa.fantasy-transit.appspot.com/map">Map</a>')
+		self.response.write('<br><h3>Reference:<h3><ul>')
+		self.response.write('<li><a href="http://nausicaa.fantasy-transit.appspot.com/map">Map</a><li></ul>')
 
-		self.response.write('<br><h3>Reference: <h3><h3>Alternate worlds:</h3><ul>')
+		self.response.write('<br><h3>Alternate worlds:</h3><ul>')
 		self.response.write('<li><a href="../">Tokyo</a></li>')
 		self.response.write('<li><a href="../alice">Alice in Wonderland</a></li>')
 		self.response.write('<li><a href="../nausicaa">Nausicaa of the Valley of the wind</a></li>')
@@ -136,7 +139,8 @@ class LotrMainPage(webapp2.RequestHandler):
 				self.response.write('<option>%s</option>' % station)
 		self.response.write('</select></h3><input type="submit" value="  Search Route  "></form>')
 
-		self.response.write('<br><h3>Reference: <h3><a href="http://lotr.fantasy-transit.appspot.com/map">Map</a>')
+		sself.response.write('<br><h3>Reference:<h3><ul>')
+		self.response.write('<li><a href="http://lotr.fantasy-transit.appspot.com/map">Map</a><li></ul>')
 
 		self.response.write('<h3>Alternate worlds:</h3><ul>')
 		self.response.write('<li><a href="../">Tokyo</a></li>')
@@ -172,7 +176,8 @@ class PokemonMainPage(webapp2.RequestHandler):
 				self.response.write('<option>%s</option>' % station)
 		self.response.write('</select></h3><input type="submit" value="  Search Route  "></form>')
 
-		self.response.write('<br><h3>Reference: <h3><a href="http://pokemon.fantasy-transit.appspot.com/map">Map</a>')
+		self.response.write('<br><h3>Reference:<h3><ul>')
+		self.response.write('<li><a href="http://pokemon.fantasy-transit.appspot.com/map">Map</a><li></ul>')
 
 		self.response.write('<h3>Alternate worlds:</h3><ul>')
 		self.response.write('<li><a href="../">Tokyo</a></li>')
